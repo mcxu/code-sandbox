4035926c3e0f21ac1f5b0c3e82fc87134d520b98
mcxu <michaelxu79@gmail.com>
2018-05-17 21:38:11 -0400

    fixed bracket alignment.

--- src/ctci5th/AlgUtils.java
+++ src/ctci5th/AlgUtils.java
@@ -2,16 +2,20 @@ package ctci5th;
 
 import java.util.HashMap;
 
-public class AlgUtils {
-
+public class AlgUtils 
+{
     public static HashMap<Character, Integer> getCharFreqInStr(String testStr) 
     {
         HashMap<Character, Integer> strFreqMap = new HashMap<>();
 
-        for (char ch : testStr.toCharArray()) {
-            if (strFreqMap.containsKey(ch)) {
+        for (char ch : testStr.toCharArray()) 
+        {
+            if (strFreqMap.containsKey(ch)) 
+            {
                 strFreqMap.put(ch, strFreqMap.get(ch) + 1);
-            } else {
+            } 
+            else 
+            {
                 strFreqMap.put(ch, 1);
             }
         }
@@ -19,11 +23,13 @@ public class AlgUtils {
         return strFreqMap;
     }
 
-    public static void test_getCharFreqInStr() {
+    public static void test_getCharFreqInStr() 
+    {
         System.out.println(getCharFreqInStr("asdff")); // {a=1, s=1, d=1, f=2}
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         test_getCharFreqInStr();
     }
 
--- src/ctci5th/DataStructures/Ch1ArraysAndStrings.java
+++ src/ctci5th/DataStructures/Ch1ArraysAndStrings.java
@@ -12,29 +12,35 @@ import ctci5th.AlgUtils;
  * 
  * @author MichaelXu
  */
-public class Ch1ArraysAndStrings {
+public class Ch1ArraysAndStrings 
+{
     /**
      * Question 1.1: Algorithm to determine if a string has all unique characters.
      * 
      * @param testStr
      */
-    public boolean strContainsAllUniqueChar(String testStr) {
+    public boolean strContainsAllUniqueChar(String testStr) 
+    {
         ArrayList<Character> compareList = new ArrayList<>();
 
-        for (int i = 0; i < testStr.length(); i++) {
+        for (int i = 0; i < testStr.length(); i++) 
+        {
             char ch = testStr.charAt(i);
             System.out.println("ch: " + ch);
-            if (compareList.contains(ch)) {
+            if (compareList.contains(ch)) 
+            {
                 System.out.println("repeat detected at index: " + i + " char: " + ch);
                 return false;
-            } else {
+            } 
+            else {
                 compareList.add(ch);
             }
         }
         return true;
     }
 
-    public void testQ1p1() {
+    public void testQ1p1() 
+    {
         boolean ch1p1Test = strContainsAllUniqueChar("adhesivesssss");
         System.out.println("ch1p1Test: " + ch1p1Test);
     }
@@ -45,16 +51,19 @@ public class Ch1ArraysAndStrings {
      * 
      * @param testStr
      */
-    public String reverseString(String testStr) {
+    public String reverseString(String testStr) 
+    {
         String out = "";
-        for (int i = testStr.length() - 1; i >= 0; i--) {
+        for (int i = testStr.length() - 1; i >= 0; i--) 
+        {
             char ch = testStr.charAt(i);
             out = out + ch;
         }
         return out;
     }
 
-    public void testQ1p2() {
+    public void testQ1p2() 
+    {
         String reversed = reverseString("qwertyuiop");
         System.out.println("reversed: " + reversed);
     }
@@ -64,7 +73,8 @@ public class Ch1ArraysAndStrings {
      * 
      * @param args
      */
-    public boolean stringsArePermutations(String str1, String str2) {
+    public boolean stringsArePermutations(String str1, String str2) 
+    {
         // build character frequency maps
         HashMap<Character, Integer> str1Freq = AlgUtils.getCharFreqInStr(str1);
         HashMap<Character, Integer> str2Freq = AlgUtils.getCharFreqInStr(str2);
@@ -75,31 +85,36 @@ public class Ch1ArraysAndStrings {
         System.out.println("keysets to compare: " + keyset1 + "\t" + keyset2);
 
         // condition to check if sets match
-        if (keyset1.containsAll(keyset2) && keyset2.containsAll(keyset1)) {
+        if (keyset1.containsAll(keyset2) && keyset2.containsAll(keyset1)) 
+        {
             Character[] ks1Array = keyset1.toArray(new Character[0]);
-            for (Character ks1Ch : ks1Array) {
-                if (str1Freq.get(ks1Ch) != str2Freq.get(ks1Ch)) {
+            for (Character ks1Ch : ks1Array) 
+            {
+                if (str1Freq.get(ks1Ch) != str2Freq.get(ks1Ch)) 
+                {
                     return false;
                 }
             }
             // return true here b/c items in set have been iterated.
             return true;
-        } else {
+        } 
+        else {
             System.out.println("keyset1 and 2 are not the same.");
             return false;
         }
     }
 
-    public void testQ1p3() {
+    public void testQ1p3() 
+    {
         boolean test1 = stringsArePermutations("qwertyuiop", "qowpeiruty");
         System.out.println("test1: " + test1);
-
+        
         boolean test2 = stringsArePermutations("asdfg", "asdfgs");
         System.out.println("test2: " + test2);
     }
 
     /**
-     * Question 1.4: Write a method to replace all spaces in a string with'%20'. You
+     * Question 1.4: Method to replace all spaces in a string with'%20'. You
      * may assume that the string has sufficient space at the end of the string to
      * hold the additional characters, and that you are given the "true" length of
      * the string.
@@ -107,22 +122,26 @@ public class Ch1ArraysAndStrings {
      * @param str
      * @return
      */
-    public String replaceSpaceWithInsert(String str, String insert) {
+    public String replaceSpaceWithInsert(String str, String insert) 
+    {
         char[] strChars = str.toCharArray();
-
+        System.out.println("before insert: " + Arrays.toString(strChars));
         for (int i = strChars.length - 1; i >= 0; i--) {
             System.out.print("i=" + i);
             char ch = strChars[i];
             System.out.print("	ch: " + ch);
 
             // only need to shift when there is a blank
-            if (ch == ' ') {
+            if (ch == ' ') 
+            {
                 System.out.print("	Blank");
                 // perform shift; repeat for every char in string to insert
-                for (int S = 0; S < insert.length() - 1; S++) {
+                for (int S = 0; S < insert.length() - 1; S++) 
+                {
                     System.out.print("\tS=" + S);
                     // shift from end of char array to current index.
-                    for (int j = strChars.length - 2; j > i; j--) {
+                    for (int j = strChars.length - 2; j > i; j--) 
+                    {
                         System.out.print("\tj=" + j);
                         strChars[j + 1] = strChars[j];
                         strChars[j] = ' ';
@@ -134,11 +153,11 @@ public class Ch1ArraysAndStrings {
 
         // for blanks insert the required string
         int k = 0;
-        for (int q = 0; q < strChars.length; q++) {
-            if (k == 3) {
-                k = 0;
-            }
-            if (strChars[q] == ' ') {
+        for (int q = 0; q < strChars.length; q++) 
+        {
+            if (k == 3) {k = 0;}
+            if (strChars[q] == ' ') 
+            {
                 strChars[q] = insert.charAt(k);
                 k++;
             }
@@ -147,13 +166,15 @@ public class Ch1ArraysAndStrings {
 
         // reconstruct char array back into string
         String result = "";
-        for (char ch : strChars) {
+        for (char ch : strChars) 
+        {
             result = result + ch;
         }
         return result;
     }
 
-    public void testQ1p4() {
+    public void testQ1p4() 
+    {
         String str = "Mr John Smith    ";
         // String str = "Mr John Smith ";
         // String str = "Mr Smith ";
@@ -161,7 +182,8 @@ public class Ch1ArraysAndStrings {
         System.out.println("result: " + result);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Ch1ArraysAndStrings ch1 = new Ch1ArraysAndStrings();
         // ch1.testQ1p1();
         // ch1.testQ1p2();
