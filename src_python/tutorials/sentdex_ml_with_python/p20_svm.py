eddf6c08feb77224fb12127ab558deab597c93ff
mcxu <michaelxu79@gmail.com>
2018-05-20 23:02:55 -0400

    update

--- src_python/tutorials/sentdex_ml_with_python/p20_svm.py
+++ src_python/tutorials/sentdex_ml_with_python/p20_svm.py
@@ -1,12 +1,10 @@
 """
-KNN is bad at handling outliers.
-
-pandas drop documentation:
-https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.drop.html
+SVM:
+Binary classifier; find decision boundary (hyperplane) between 2 sets.
 """
 
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
+from sklearn import preprocessing, cross_validation, neighbors, svm
 import pandas as pd
 
 pd.set_option('display.max_columns', 20)
@@ -22,7 +20,7 @@ y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier(n_jobs=2)
+clf = svm.SVC()
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
@@ -33,7 +31,4 @@ example_measures = np.array([[4,2,1,1,1,2,3,2,1],
 example_measures = example_measures.reshape(2,-1) 
 prediction = clf.predict(example_measures)
 print("prediction: ", prediction)
-
-
-
 
