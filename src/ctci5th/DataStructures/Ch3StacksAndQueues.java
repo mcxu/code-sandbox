813897d9d564f2daedfe7835a95897bc933802a7
mxu <michaelxu79@gmail.com>
2018-06-03 11:37:37 -0400

    update

--- src/ctci5th/DataStructures/Ch3StacksAndQueues.java
+++ src/ctci5th/DataStructures/Ch3StacksAndQueues.java
@@ -2,10 +2,7 @@ package ctci5th.DataStructures;
 
 public class Ch3StacksAndQueues 
 {
-    public void oneArray3Stacks()
-    {
-        
-    }
+    
     
     public static void main(String[] args) 
     {
