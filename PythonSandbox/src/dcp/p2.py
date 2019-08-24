b67c073090335e7f169d74ef1ba2da6168930305
mcxu <michaelxu79@gmail.com>
2019-08-24 09:05:55 -0400

    Adding p2

---
+++ PythonSandbox/src/dcp/p2.py
@@ -0,0 +1,41 @@
+'''
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+'''
+
+class P2():
+    
+    def eval(self, a):
+        output = []
+        for i in range(len(a)):
+            p = []
+            for j in range(len(a)):
+                if(j != i):
+                    p.append(a[j])
+            print("p: {}".format(p))
+            
+            # take product of p
+            prod = p[0]
+            for k in range(1, len(p)):
+                prod *= p[k]
+            print("prod: {}".format(prod))
+            
+            # append prod to output
+            output.append(prod)
+        return output
+
+
+if __name__ == "__main__":
+    p2 = P2()
+    a = [1,2,3,4,5]
+    out1 = p2.eval(a)
+    print("out1: {}".format(out1))
+    
+    b = [3,2,1]
+    out2 = p2.eval(b)
+    print("out2: {}".format(out2))
