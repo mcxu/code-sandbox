9d84913406e23e3bf9cb13537b1660b955c1a039
mcxu <michaelxu79@gmail.com>
2019-09-14 10:58:37 -0400

    update 9-14-2019

--- PythonSandbox/src/dcp/p4.py
+++ PythonSandbox/src/dcp/p4.py
@@ -17,7 +17,32 @@ import math
 class P4():
 
     def first_missing_pos_int(self, input):
-        pass
+        input = self.split_and_sort_positives(input)
+        print("first_missing_pos_int: split_and_sort_positives: {}".format(input))
+
+        # handle case where lowest post int is inside array
+        for i in range(len(input)-1):
+            next_consecutive = input[i] + 1
+            next_actual = input[i + 1]
+            if(next_actual > next_consecutive):
+                return next_consecutive
+
+        # handle case where lowest pos int is outside array
+        return input[len(input)-1] + 1
+
+    def split_and_sort_positives(self, input):
+        # split positive and negative numbers
+        i=0
+        while i < len(input):
+            print("sort_positives")
+            num = input[i]
+            if num < 0:
+                input.pop(i)
+                i -= 1
+            i += 1
+        pos_sorted = self.counting_sort(input)
+        print("pos_sorted: {}".format(pos_sorted))
+        return pos_sorted
 
     def counting_sort(self, input):
         # identify highest value in input
@@ -58,6 +83,20 @@ class P4():
 
         return sa[1:len(input)+1]
 
+    
+    # def counting_sort_negatives(self, input):
+    #     input_abs = [abs(x) for x in input]
+    #     print("input_abs: {}".format(input_abs))
+    #     sorted_abs = self.counting_sort(input_abs)
+    #     print("sorted_abs: {}".format(sorted_abs))
+
+    #     # reverse and make negative
+    #     sorted_negatives = []
+    #     for i in range(len(sorted_abs)-1, -1, -1):
+    #         sorted_negatives.append(-sorted_abs[i])
+    #     print("sorted_negatives: {}".format(sorted_negatives))
+    #     return sorted_negatives
+
     def test_counting_sort(self):
         input_lists = [
             [5,2,7,1],
@@ -68,15 +107,26 @@ class P4():
             a = self.counting_sort(input)
             print("test_counting_sort: input: {}, output: {}".format(input, a))
 
+    def test_counting_sort_negatives(self):
+        l = [-2,-12,-3,-7]
+        self.counting_sort_negatives(l)
 
-    def test_first_missing_pos_int(self):
-        pass
+    def test_first_missing_pos_int_1(self):
+        l = [-1, -3, 45, 2, -7, 23, 4, 7, 2, 4]
+        fmpi = self.first_missing_pos_int(l)
+        print("test_first_missing_pos_int: fmpi: {}".format(fmpi))
+    
+    def test_first_missing_pos_int_2(self):
+        l = [-4, -2, -8, 5,3,4,1,2]
+        fmpi = self.first_missing_pos_int(l)
+        print("test_first_missing_pos_int: fmpi: {}".format(fmpi))
 
 def main():
     p4 = P4()
-    p4.test_counting_sort()
-    #p4.test_first_missing_pos_int()
-
+    #p4.test_counting_sort()
+    #p4.test_counting_sort_negatives()
+    #p4.test_first_missing_pos_int_1()
+    p4.test_first_missing_pos_int_2()
 
 if __name__ == "__main__":
     main()
