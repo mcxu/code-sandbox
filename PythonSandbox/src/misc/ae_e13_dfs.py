cdf22d8a5631ace642119f185c6451b4cb144845
mcxu <michaelxu79@gmail.com>
2019-10-16 23:41:07 -0400

    update 1 10-16-2019

--- PythonSandbox/src/misc/ae_e13_dfs.py
+++ PythonSandbox/src/misc/ae_e13_dfs.py
@@ -2,6 +2,7 @@
 Algoexpert - Easy - 2/13 
 Depth First Search
 """
+import time 
 
 class Node:
     def __init__(self, name):
@@ -10,35 +11,55 @@ class Node:
 
         # stack list
         self.stack = []
+        self.currChildren = None
 
     def addChild(self, name):
         self.children.append(Node(name))
         return self
 
     # need to populate input array
-    # iterative solution
-    def depthFirstSearch(self, array):
+    def depthFirstSearchIterative(self, array):
         if not self.children:
             return array
 
         # push a name onto stack
-        self.stackPush(self.name)
+        self.stackPush(self)
+        print("pushed Item on stack: ", self.stack)
+        print("starting item: {}, with name: {}".format(self.stack[0], self.stack[0].name))
 
         while(self.stack):
-            pass        
+            print("--- top of while loop ---")
+            poppedNode = self.stackPop()
+            self.currChildren = poppedNode.children
+            poppedName = poppedNode.name
+            print("poppedName:", poppedName)
 
-
-        
+            # put name on array if not visited
+            if poppedName not in array:
+                array.append(poppedName)
+                print("elt added to array:", array)
+            
+            # add un-visited immediate children to stack
+            for i in range(len(self.currChildren)-1, -1, -1):
+                childNode = self.currChildren[i]
+                childName = childNode.name
+                print("childName:", childName)
+                if childName not in array:
+                    self.stackPush(childNode)
+                    print("unvisited child added to stack:", self.stack)
+            
+            time.sleep(.25)
+        return array
 
     def stackPush(self, name):
         self.stack.append(name)
     
     def stackPop(self):
-        return self.stack.pop(-1)
+        return self.stack.pop()
 
 class Test:
     # The one given in prompt
-    def testTree1(self):
+    def promptTree(self):
         tree = Node("A")
         tree.addChild("B").addChild("C").addChild("D")
         print("{} {} {}".format(tree.children[0].name, tree.children[1].name, tree.children[2].name))
@@ -46,11 +67,55 @@ class Test:
         tree.children[0].children[1].addChild("I").addChild("J") # from F
         tree.children[2].addChild("G").addChild("H") # from D
         tree.children[2].children[0].addChild("K") # from G
-        
+        return tree
+    
+    def testTree1(self):
+        test1 = Node("A")
+        test1.addChild("B").addChild("C")
+        test1.children[0].addChild("D")
+        return test1
+    
+    def testTree5(self):
+        test5 = Node("A")
+        test5.addChild("B").addChild("C").addChild("D").addChild("L").addChild("M")
+        test5.children[0].addChild("E").addChild("F").addChild("O")
+        test5.children[1].addChild("P")
+        test5.children[2].addChild("G").addChild("H")
+        test5.children[0].children[0].addChild("Q").addChild("R")
+        test5.children[0].children[1].addChild("I").addChild("J")
+        test5.children[2].children[0].addChild("K")
+        test5.children[4].addChild("S").addChild("T").addChild("U").addChild("V")
+        test5.children[4].children[0].addChild("W").addChild("X")
+        test5.children[4].children[0].children[1].addChild("Y").addChild("Z")
+        return test5
+
+    def test_depthFirstSearchIterative_prompt(self):
+        tree = self.promptTree()
+        array = []
+        result = tree.depthFirstSearchIterative(array)
+        print("test_depthFirstSearchIterative_Given: result: ", result)
+
+    def test_depthFirstSearchIterative1(self):
+        tree = self.testTree1()
+        array = []
+        result = tree.depthFirstSearchIterative(array)
+        print("test_depthFirstSearchIterative2: result: ", result)
+
+    def test_depthFirstSearchIterative5(self):
+        tree = self.testTree5()
+        result = tree.depthFirstSearchIterative([])
+        print("test_depthFirstSearchIterative5: result: ", result)
+        ans = ["A", "B", "E", "Q", "R", "F", "I", "J", "O", "C", "P", "D", "G", "K", "H", "L", "M", "S", "W", "X", "Y", "Z", "T", "U", "V"]
+        if result == ans:
+            print("test_depthFirstSearchIterative5: result matches answer!")
+
 
 def main():
     test = Test()
-    test.testTree1()
+    #test.testTree1()
+    #test.test_depthFirstSearchIterative_prompt()
+    #test.test_depthFirstSearchIterative2()
+    test.test_depthFirstSearchIterative5()
 
 if __name__ == "__main__":
     main()
