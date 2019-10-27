0c1d7b62928862749e2e0cf0ecd9cefb666740a6
mcxu <michaelxu79@gmail.com>
2019-10-27 13:47:26 -0400

    update 1 10-27-2019

---
+++ PythonSandbox/src/ae/ae_nth_fibonacci.py
@@ -0,0 +1,55 @@
+class Fibonacci:
+
+    def __init__(self):
+        # map of fib index to the fib number
+        self.fibMap = {}
+
+    # index of 0 is 1
+    def getNthFib(self, n):
+        #print("n= ", n)
+        # count starting from 0
+        # if n == 0 or n == 1:
+        #     return n
+        
+        # error check
+        if n < 1:
+            return 0
+
+        # count starting from 1
+        if n == 1 or n == 2:
+            return n-1
+
+        return self.getNthFib(n-1)+self.getNthFib(n-2)
+
+    def test_getNthFib(self):
+        a = self.getNthFib(6)
+        print("a: ", a)
+
+
+    def getNthFib_memoized(self, n):
+        # error check
+        if n < 1:
+            return 0
+
+        # count starting from 1
+        if n == 1 or n == 2:
+            return n-1
+
+        if n in self.fibMap.keys():
+            print("n={} in fibMap".format(n))
+            return self.fibMap[n]
+            
+        self.fibMap[n] = self.getNthFib_memoized(n-1)+self.getNthFib_memoized(n-2)
+        return self.fibMap[n]
+
+    def test_getNthFib_memoized(self):
+        a = self.getNthFib_memoized(100)
+        print("a: ", a)
+
+def main():
+    f = Fibonacci()
+    #f.test_getNthFib()
+    f.test_getNthFib_memoized()
+
+if __name__ == "__main__":
+    main()
