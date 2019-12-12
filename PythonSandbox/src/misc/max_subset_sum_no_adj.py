f204d1483b0ad55dd23339d2134ee4980439a8ff
Michael Xu <michaelxu79@gmail.com>
2019-12-12 14:30:18 -0500

    update

--- PythonSandbox/src/misc/max_subset_sum_no_adj.py
+++ PythonSandbox/src/misc/max_subset_sum_no_adj.py
@@ -8,20 +8,40 @@ Sample output: 330 (75, 120, 135)
 """
 
 class MS:
+    """
+    inclPrev: max sum including previous element
+    exclPrev: max sum excluding previous element
+    """
     @staticmethod
     def maxSubsetSumNoAdjacent(array):
-        # record the index:value of the original array
-        indexMap = {}
-        for i,v in enumerate(array):
-            indexMap[i] = v
-        print("indexMap filled:", indexMap)
+        if not array:
+            return 0
 
-        aSorted = sorted(array)
-        print("aSorted:", aSorted)
+        inclPrev = array[0]
+        exclPrev = 0
 
+        for i in range(1, len(array)):
+            v = array[i]
+            print("i={}, v={}".format(i,v))
+            inclPrevPrevIter = inclPrev
+            exclPrevPrevIter = exclPrev
+            print("inclPrevPrevIter: {}, exclPrevPrevIter: {}".format(inclPrevPrevIter, exclPrevPrevIter))
+            inclPrev = (exclPrev + v)
+            exclPrev = max(inclPrevPrevIter, exclPrevPrevIter)
+            print("inclPrev: {}, exclPrev: {}".format(inclPrev, exclPrev))
+        
+        return max(inclPrev, exclPrev)
+            
     @staticmethod
     def test_maxSubsetSumNoAdjacent():
         a = [75, 105, 120, 75, 90, 135]
         MS.maxSubsetSumNoAdjacent(a)
+    
+    @staticmethod
+    def test_maxSubsetSumNoAdjacent2():
+        a = [1, 15, 3]
+        max = MS.maxSubsetSumNoAdjacent(a)
+        print("max: ", max)
 
-MS.test_maxSubsetSumNoAdjacent()
+#MS.test_maxSubsetSumNoAdjacent()
+MS.test_maxSubsetSumNoAdjacent2()
