23e7f142c04f1d5ee9491f4d88fe5b32d6381b6f
mcxu <michaelxu79@gmail.com>
2020-01-05 21:19:25 -0500

    update

--- PythonSandbox/src/misc/levenshtein_distance.py
+++ PythonSandbox/src/misc/levenshtein_distance.py
@@ -10,8 +10,41 @@ Sample output: 2 (insert "y", subsitute "c" for "d")
 class Prob:
     @staticmethod
     def levenshteinDistance(str1, str2):
-        pass
+        # 2d array to store number of ops
+        ops = [[-1 for i in range(len(str1)+1)] for j in range(len(str2)+1)]
+        print("ops: ", ops)
+        
+        # populate null row and column (convention from row to column)
+        ops[0] = [i for i in range(len(str1)+1)]
+        for i in range(1, len(ops)):
+            ops[i][0] = i
+        print("ops after init: ", ops)
+        
+        # iterate through each subproblem in the 2d array
+        for i in range(1, len(ops)):
+            for j in range(1, len(ops[i])):
+                print("i= %d, j= %d" % (i, j),)
+                
+                fromChar = str1[j-1]
+                toChar = str2[i-1]
+                print("    fromChar: %s, toChar: %s" % (fromChar, toChar))
+                
+                replace = ops[i-1][j-1]
+                delete = ops[i-1][j]
+                insert = ops[i][j-1]
+                print("    replace: %s, delete: %s, insert: %s" % (replace, delete, insert))
+                
+                if fromChar == toChar:
+                    ops[i][j] = replace
+                else:
+                    ops[i][j] = min(replace, delete, insert) + 1
+        
+        print("ops final: ", ops)
+        return ops[len(str2)][len(str1)]
 
     @staticmethod
     def test1():
-        Prob.levenshteinDistance("abc", "yabd")
+        minDist = Prob.levenshteinDistance("abc", "yabd")
+        print("test1: minDist: ", minDist)
+
+Prob.test1()
