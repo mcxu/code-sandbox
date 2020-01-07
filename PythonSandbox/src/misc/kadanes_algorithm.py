d62382305a70189c917d7b4266192104dfc49765
mcxu <michaelxu79@gmail.com>
2020-01-06 23:31:19 -0500

    update

--- PythonSandbox/src/misc/kadanes_algorithm.py
+++ PythonSandbox/src/misc/kadanes_algorithm.py
@@ -9,59 +9,55 @@ Sample output: 19 ([1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1])
 
 class KD:
     
-    # TODO: Not completely correct. Need to fix.
+#     @staticmethod
+#     def kadanesAlgorithm(array):
+#         pass
+    
+    
+    """
+    Brute force:
+    Time complexity: O(n^2), where n=len(array)
+    Space complexity: O(1), because maxSum only stores the highest sum so far
+    """
     @staticmethod
-    def kadanesAlgorithm(array):
-        # array for storing sum from ith value
-        sums = [0 for i in range(len(array))]
-        print("sums init: ", sums)
-        
-        maxSub = sums
+    def maxSumSubArrayBruteForce(array):
+        maxSum = -float("inf")
         for i in range(len(array)):
             val = array[i]
             print("i= %s, val= %s" % (i ,val))
-            print("    rest of array: ", array[i:])
-            print("    sum: ", sum(array[i:]))
             
-            if sum(array[i:]) > max(sums):
-                maxSub = array[i:]
-                print("    maxSub updated to: ", maxSub)
-            
-            sums[i] = sum(array[i:])
-            print("    sums: ", sums)
-            
-        print("sums after: ", sums)
-        print("maxSub: ", maxSub)
-        maxSum = max(sums)
-        for i in range(len(maxSub), -1, -1):
-            print("i = ", i)
-            print("    maxSub: ", maxSub[0:i])
-            print("    sum of maxSub: ", sum(maxSub[0:i]))
-            if sum(maxSub[0:i]) > max(sums):
-                maxSum = sum(maxSub[0:i])
-                print("    maxSum updated to: ", maxSum)
-        
-        print("maxSum: ", maxSum)
+            for j in range(i+1, len(array)+1):
+                print("    j=", j)
+                subarray = array[i:j]
+                print("        subarray: ", subarray)
+                saSum = sum(subarray)
+                print("        saSum: ", saSum)
+                if saSum > maxSum:
+                    maxSum = saSum
+                    print("        maxSum set to: ", maxSum)
         return maxSum
                 
     
     @staticmethod
     def test1():
-        array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
-        KD.kadanesAlgorithm(array)
+        array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4] #ans: 19
+        ans = KD.kadanesAlgorithm(array)
+        print("test1 ans: ", ans)
     
     @staticmethod
     def test2():
         array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #ans: 55
-        KD.kadanesAlgorithm(array)
+        ans = KD.kadanesAlgorithm(array)
+        print("test2 ans: ", ans)
     
     @staticmethod
     def test3():
         array = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10] #ans: -1
-        KD.kadanesAlgorithm(array)
+        ans = KD.kadanesAlgorithm(array)
+        print("test3 ans: ", ans)
         
 #KD.test1()
 #KD.test2() 
-KD.test3()      
+#KD.test3()      
 
     
