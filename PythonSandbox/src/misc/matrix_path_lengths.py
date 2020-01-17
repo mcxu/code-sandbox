6e046a1573238db663b0df952c98244890398462
mcxu <michaelxu79@gmail.com>
2020-01-16 23:05:54 -0500

    update: matrix path lengths

--- PythonSandbox/src/misc/matrix_path_lengths.py
+++ PythonSandbox/src/misc/matrix_path_lengths.py
@@ -12,8 +12,7 @@ Sample input:
 
 Sample output: [1,2,2,2,5]
 '''
-import time
-   
+
 class Prob:
     @staticmethod
     def getLengths(matrix):
@@ -38,6 +37,7 @@ class Prob:
                     Prob.doBfs(j, i, matrix, aux, paths)
         
         print("!!!! paths: ", paths)
+        return paths
                         
                         
     @staticmethod
@@ -53,26 +53,27 @@ class Prob:
             # check if currCoord has been visited
             visitedState = aux[currj][curri]
             print("    currCoord has been visited: ", visitedState)
-#             if visitedState:
-#                 continue
             
-            # label currCoord as visited
-            aux[currj][curri] = True
-            print("    aux now: ", aux)
+            if visitedState == False:
+                # label currCoord as visited
+                aux[currj][curri] = True
+                print("    aux[j={}][i={}] set to True: ".format(currj,curri))
             
-            currVal = matrix[currj][curri]
-            print("    currVal: ", currVal)
-            if currVal >= 1:
-                pathLength += currVal
-            else:
-                continue
-            
-            # search neighbors
-            unvisiteds = Prob.getUnvisitedNeighbors(currj, curri, matrix, aux)
-            queue += unvisiteds
+                currVal = matrix[currj][curri]
+                print("    currVal: ", currVal)
+                if currVal == 1:
+                    pathLength += currVal
+                    print("    increment pathLength to: ", pathLength)
+                    
+                    # search neighbors
+                    unvisiteds = Prob.getUnvisitedNeighbors(currj, curri, matrix, aux)
+                    queue += unvisiteds
+                    
             print("    queue: ", queue)
-            time.sleep(.5)
-        paths.append(pathLength)
+            
+        if pathLength >= 1:
+            paths.append(pathLength)
+        print("    paths for ({},{}): {}".format(j, i, paths))
             
     @staticmethod
     def getUnvisitedNeighbors(j, i, matrix, aux):
@@ -92,7 +93,7 @@ class Prob:
             unvisiteds.append([j,i-1])
         
         # check right
-        if i < (len(matrix)-1) and aux[j][i+1] == False:
+        if i < (len(matrix[0])-1) and aux[j][i+1] == False:
             unvisiteds.append([j,i+1])
         
         print("        univisiteds: ", unvisiteds)
@@ -112,10 +113,22 @@ class Prob:
         # auxilary matrix to store visited state (False: unvisited, True: visited)
         aux = [[False for i in row] for row in matrix]
         print("aux: ", aux)
-        j=1
+        j=3
         i=2
         Prob.doBfs(j, i, matrix, aux, paths)
         print("paths is now: ", paths)
+    
+    @staticmethod
+    def test_doBfs2():
+        matrix = [
+            [1,1,1],
+            [0,1,1],
+            [1,1,0]]
+        paths = []
+        aux = [[False for i in row] for row in matrix]
+        j = 1
+        i = 1
+        Prob.doBfs(j, i, matrix, aux, paths)
     
     @staticmethod
     def test1():
@@ -126,8 +139,65 @@ class Prob:
             [1,0,1,0,1],
             [1,0,1,1,0]]
         Prob.getLengths(matrix)
-
-#Prob.test1()
-Prob.test_doBfs()           
+    
+    @staticmethod
+    def test2():
+        matrix = [[0]]
+        Prob.getLengths(matrix)
         
+        matrix = [[1]]
+        Prob.getLengths(matrix)
+    
+    @staticmethod
+    def test3():
+        """ correct: [4,2]
+        getting: [6]
+        """
+        matrix = [
+            [1,1,1],
+            [0,0,1],
+            [1,1,0]]
+        Prob.getLengths(matrix)
+    
+    @staticmethod
+    def test4():
+        matrix = [[1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0]]
+        expected = [1, 2, 3]
+        Prob.getLengths(matrix)
+    
+    @staticmethod
+    def test_doBfs3():
+        matrix = [
+            [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
+            [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+            [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+        ]
+        paths = []
+        aux = [[False for i in row] for row in matrix]
+        j = 2
+        i = 4
+        Prob.doBfs(j, i, matrix, aux, paths)
+    
+    @staticmethod
+    def test5():
+        matrix = [
+            [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
+            [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+            [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+        ]
+        Prob.getLengths(matrix)
+
+#Prob.test_doBfs()
+#Prob.test1()
+#Prob.test2()
+#Prob.test3()
+#Prob.test_doBfs2()
+#Prob.test4()
+#Prob.test_doBfs3()  
+Prob.test5()
+
         
