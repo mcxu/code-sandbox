60979c05afff9c2443d52248ac8c4cebbed83c9e
mcxu <michaelxu79@gmail.com>
2020-02-12 00:04:19 -0500

    updated largest consecutive range to take up less memory

--- PythonSandbox/src/misc/largest_consecutive_range.py
+++ PythonSandbox/src/misc/largest_consecutive_range.py
@@ -7,9 +7,12 @@ which represents the largest range of consecutive numbers in the array.
 Sample input: [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
 Sample output: [0, 7]
 '''
-import time
 
 class Prob:
+    '''
+    Time complexity: O(n) since all of these loops and the max() iterate through entire array.
+    Space complexity: O(n) since the rangeMap stores at most n keys and n values.
+    '''
     @staticmethod
     def largestRange(array):
         rangeMap = {}
@@ -17,7 +20,7 @@ class Prob:
         for v in array:
             nextVal = v + 1
             if nextVal in array:
-                rangeMap[v] = [nextVal]
+                rangeMap[v] = nextVal
         print("rangeMap: ", rangeMap)
         
         if not rangeMap:
@@ -26,26 +29,24 @@ class Prob:
         
         for v in array:
             if v in rangeMap.keys():
-                i = 0
-                while i < len(rangeMap[v]):
-                    print("rangeMap v length: ", len(rangeMap[v]))
-                    nextVal = rangeMap[v][-1]
-                    if nextVal in rangeMap.keys():
-                        rangeMap[v] += rangeMap[nextVal]
+                nextVal = v+1
+                while nextVal in rangeMap.keys():
+                    rangeMap[v] = rangeMap[nextVal]
                     print("rangeMap B: ", rangeMap)
-                    i += 1
+                    nextVal += 1
         print("rangeMap C: ", rangeMap)
         
-        maxRangeKey = max(rangeMap.keys(), key= lambda x: len(rangeMap[x]))
+        maxRangeKey = max(rangeMap.keys(), key= lambda x: rangeMap[x] - x)
         print("maxRangeKey: ", maxRangeKey)
-        return [maxRangeKey, rangeMap[maxRangeKey][-1]]
+        return [maxRangeKey, rangeMap[maxRangeKey]]
     
     @staticmethod
     def test1():
         #array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
         #array = [1]
         #array = [1,3,5]
-        array = [1,3,5,6,7,9,10,11,12]
+        #array = [1,3,5,6,7,9,10,11,12]
+        array = [3,4,5,6,7]
         r = Prob.largestRange(array)
         print("test1: r: ", r)
 
