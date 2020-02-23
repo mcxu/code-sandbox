6ed0d0bd26576f5df99f48972fa70803cfa9ddf7
mcxu <michaelxu79@gmail.com>
2020-02-23 17:33:41 -0500

    updates

--- PythonSandbox/src/misc/max_path_sum_binary_tree.py
+++ PythonSandbox/src/misc/max_path_sum_binary_tree.py
@@ -12,6 +12,7 @@ Sample output: 18 (sum(5,2,1,3,7))
 
 https://leetcode.com/problems/binary-tree-maximum-path-sum/
 https://www.youtube.com/watch?v=mOdetMWwtoI&t=725s
+https://www.youtube.com/watch?v=peyO-Nu1jGc
 '''
 
 class BTNode():
@@ -21,6 +22,7 @@ class BTNode():
         self.right = None
 
 class Prob:
+    
     @staticmethod
     def maxPathSum(tree):
         # The var for storing the max path sum. This begins with -inf to ensure the algorithm works
@@ -36,32 +38,72 @@ class Prob:
             print("tree value: ", tree.value) 
             print("    mps: ", mps)
             
+            mpsUpToLeftChild = currPathSum(tree.left)
+            mpsUpToRightChild = currPathSum(tree.right)
+            
+            # This is picking a singular path from 3 choices: (1) new path starting from tree.value if adding 
+            # mpsUpToLeftChild or mpsUpToRightChild actually results in decreasing the tree.value.
+            # (2) Path from left child to value. (3) Path from right child to value.
+            mpsUpToCurrNode = max(tree.value, tree.value+mpsUpToLeftChild, tree.value+mpsUpToRightChild)
+            
+            # This is determining where the uppermost node is in the path, since at this node, 
+            # the root value must be added to the left and right MPS. So if this is greater than 
+            # The mpsUpToCurrNode (for a singular path), then it must be the new "triangle".
+            mpsTriangle = max(mpsUpToCurrNode, mpsUpToLeftChild + tree.value + mpsUpToRightChild)
+            
+            # If the new triangle is greater than the current mps, then update
+            mps = max(mps, mpsTriangle)
+            
+            # Return mpsUpToCurrNode because the recursive calls require the greatest PATH sum,
+            # whether its left->root, or right->root. (child->root since this is postorder traversal.)
+            # It's not asking for the triangle, but the PATH.
+            return mpsUpToCurrNode
+         
+        currPathSum(tree)
+        return mps
+
+
+    @staticmethod
+    def maxPathSum2(tree):
+        # The var for storing the max path sum. This begins with -inf to ensure the algorithm works
+        # for trees with only negative numbers.
+        mps = -float('inf')
+          
+        def currPathSum(tree):
+            nonlocal mps
+             
+            # If no node, then the max path sum to that node is 0
+            if tree == None:
+                return 0
+            print("tree value: ", tree.value) 
+            print("    mps: ", mps)
+             
             # Do postorder traversal; access the root AFTER recursively calling left and right.
             # This is so that for each node, you have the previously computed mps for left and right
             # children, and work your way up. The comparison with 0 is so that if the current
             # node's tree.value is negative, it should not be included in the path.
             mpsUpToLeftChild = max(0, currPathSum(tree.left))
             mpsUpToRightChild = max(0, currPathSum(tree.right))
-            
+             
             # If the current sums up to each child + the node value is greater than previous mps,
             # then the current mps becomes that. Otherwise, say in the case where tree.value is negative,
             # the old mps is still the greatest.
             mps = max(mps, mpsUpToLeftChild + tree.value + mpsUpToRightChild)
             print("    mps updated: {} for tree value: {}".format(mps,tree.value))
-            
+             
             # Determine which path up to child + tree.value should be chosen
             mpsUpToCurrNode = max(mpsUpToLeftChild, mpsUpToRightChild) + tree.value
             print("    mpsUpToCurrNode: {} for tree value: {}".format(mpsUpToCurrNode, tree.value))
-            
+             
             return mpsUpToCurrNode
-         
+          
         currPathSum(tree)
         return mps
 
     @staticmethod
     def test1():
-        #tree = BTNode(1) # correct ans: 18
-        tree = BTNode(-1) # correct ans: 16
+        tree = BTNode(1) # correct ans: 18
+        #tree = BTNode(-1) # correct ans: 16
         tree.left = BTNode(2)
         tree.left.left = BTNode(4)
         tree.left.right = BTNode(5)
@@ -95,8 +137,8 @@ class Prob:
         s = Prob.maxPathSum(tree)
         print("test4: s: ", s)
 
-Prob.test1()
+#Prob.test1()
 #Prob.test2() 
 #Prob.test3()
-#Prob.test4()
+Prob.test4()
 
