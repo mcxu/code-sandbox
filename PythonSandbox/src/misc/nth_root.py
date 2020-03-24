09817ceab7bb8a481b284af54089b32a6e743666
mcxu <michaelxu79@gmail.com>
2020-03-23 23:02:30 -0400

    adding explanation to nth root

--- PythonSandbox/src/misc/nth_root.py
+++ PythonSandbox/src/misc/nth_root.py
@@ -4,27 +4,31 @@ Find the nth root of value x.
 
 class Prob:
     '''
-    
+    Using binary search.
+    Time complexity: O(log_2(x)), since the binary search searches initially using [0,x]
+    Space complexity: O(1), since the med variable just stores the median after each iteration.
     '''
     @staticmethod
     def nthRoot(n, x):
         lo = 0
         hi = x
-        acc = .00000000001 # accuracy of calculation
+        acc = .000001 # accuracy of calculation
         med = (lo + hi)/2
-        while abs(med**2 - x) > acc:
+        i = 0
+        while abs(med**n - x) > acc:
+            print("med: ", med)
             if med**n > x:
                 hi = med
-            elif med**n < x:
+            else:
                 lo = med
             med = (lo + hi)/2
-        
+            i += 1
         return med
     
     @staticmethod
     def test1(alg):
-        n = 2 # root
-        x = 3.14
+        n = 3 # root
+        x = 1001
         r = alg(n, x)
         print("r: ", r)
 
