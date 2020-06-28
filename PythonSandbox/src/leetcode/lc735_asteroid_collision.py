2d18e812913f5e556ff7b1ea629e87a434a39731
mcxu <michaelxu79@gmail.com>
2020-06-27 20:55:03 -0400

    added lc735 asteroid collision

---
+++ PythonSandbox/src/leetcode/lc735_asteroid_collision.py
@@ -0,0 +1,43 @@
+'''
+https://leetcode.com/problems/asteroid-collision/
+We are given an array asteroids of integers representing asteroids in a row.
+For each asteroid, the absolute value represents its size, and the sign represents its direction 
+(positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. 
+If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+'''
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for i,v in enumerate(asteroids):
+            #print("v: ", v)
+            if not stack:
+                stack.append(v)
+            else:
+                if v>0 or (v<0 and stack[-1]<0):
+                    stack.append(v)
+                else:
+                    if abs(v)>abs(stack[-1]):
+                        stack[-1] = v
+                    elif abs(v)==abs(stack[-1]):
+                        stack.pop()
+            
+            self.removeCollisions(stack)
+        
+        return stack
+    
+    # mutates stack to remove collisions 
+    def removeCollisions(self, stack):              
+        j = len(stack)-1
+        while stack and stack[j-1]>0 and stack[j]<0:
+            #print("stack while: ", stack)
+            if abs(stack[j-1])>abs(stack[j]):
+                stack.pop()
+            elif abs(stack[j-1])==abs(stack[j]):
+                stack.pop(j-1)
+                stack.pop()
+                j -= 1
+            else:
+                stack[j-1]=stack[j]
+                stack.pop()
+            j -= 1
