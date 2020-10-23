c26b97234a5092cb031211212659acf0430f7d54
mcxu <michaelxu79@gmail.com>
2020-10-22 20:34:02 -0400

    lc19 remove nth node from end of linked list

---
+++ PythonSandbox/src/leetcode/lc19_remove_nth_node_end_of_list.py
@@ -0,0 +1,31 @@
+
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        nodeMap = {}
+        i = 0
+        m = head
+        while m != None:
+            nodeMap[i] = m
+            if len(nodeMap) > n+1:
+                nodeMap.pop(i-n-1)
+            i += 1
+            m = m.next
+        
+        # remove using map
+        maxInd = max(nodeMap.keys())
+        nthInd = maxInd-n+1
+        if nthInd-1 >= 0:
+            nodeMap[nthInd-1].next = nodeMap[nthInd].next
+        
+        if nodeMap[nthInd] == head:
+            head = head.next
+        nodeMap[nthInd].next = None
+        nodeMap.pop(nthInd)
+        
+        return head
