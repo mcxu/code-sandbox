bee0ab9083fdaefa83ea594c393448b8b479ff6b
mcxu <michaelxu79@gmail.com>
2023-01-19 18:40:04 -0500

    LC14

--- PythonSandbox/src/leetcode/lc14_longest_common_prefix.py
+++ PythonSandbox/src/leetcode/lc14_longest_common_prefix.py
@@ -2,40 +2,67 @@
 
 class Solution:
     # Using trie.
-    def longestCommonPrefix2(self, strs: [str]) -> str:
-        trie, shortest = self.buildTrie(strs)
+    def longestCommonPrefix(self, strs: [str]) -> str:
+        trieRoot, shortestStrLen = self.buildTrie(strs)
         
-        # traverse the trie to build the common prefix
-        n = trie
-        commonPref = ""
-        while len(n.keys())==1 and shortest>0:
-            k = list(n.keys())[0]
-            commonPref += k
-            n = n[k]
-            shortest -= 1
-        return commonPref
-    
-    def buildTrie(self, strs):
-        trie = {}
-        shortest = float('inf') # length of the shortest word
-        for i,word in enumerate(strs):
-            if len(word) < shortest:
-                shortest = len(word)
+        # build the shortest prefix  
+        ptr = trieRoot
+        builtPrefix = ""
+        while len(ptr.keys()) == 1 and len(builtPrefix) < shortestStrLen:
+            currKey = list(ptr.keys())[0]
+            builtPrefix += currKey
+            ptr = ptr[currKey]
+        
+        return builtPrefix
+                    
+                        
+    def buildTrie(self, strs, shortestStrLen = float('inf')):
+        trieRoot = {}
+
+        for string in strs:
             
-            n = trie
-            if word:  
-                for j,ch in enumerate(word):
-                    if ch not in n.keys():
-                        n[ch] = {}
-                    n = n[ch]
+            if string == "":
+                return {}, 0
             else:
-                n[word] = {}
-                
-        return trie, shortest
+                shortestStrLen = min(shortestStrLen, len(string))
+
+            ptr = trieRoot
+            for char in string:
+                if char not in ptr.keys():
+                    ptr[char] = {}
+                ptr = ptr[char]
+
+        return trieRoot, shortestStrLen
+    
+    # ========== test ==========
+
+    def doDFS(self, trieRoot, currPath: str, allPaths: set):
+        if trieRoot == None:
+            return trieRoot
+        
+        if not trieRoot:
+            allPaths.add(currPath)
+            return
+
+        for child in trieRoot.keys():
+            self.doDFS(trieRoot[child], currPath+child, allPaths)
+
+    def test(self):
+        cases = [
+            dict(strs= ["flower","flow","flight"], output= "fl"),
+            dict(strs= ["dog","racecar","car"], output = ""),
+            dict(strs = ["ab", "a"], output = "a")
+        ]
+
+        for c in cases:
+            res = self.longestCommonPrefix(c["strs"])
+            print("case: ", c)
+            print("res: ", res)
+            assert res == c["output"]
 
     # ==========================================================
 
-    def longestCommonPrefix(self, strs: [str]) -> str:
+    def longestCommonPrefix_1stAttempt(self, strs: [str]) -> str:
         if not strs:
             return ""
         
@@ -68,3 +95,6 @@ class Solution:
                 refChar = word[j]
                 
         return commonPref
+
+sol = Solution()
+sol.test()
